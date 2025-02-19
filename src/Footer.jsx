a9cbdf65105456ca8f7e9bb90ab289d3693500bf
Mads Hartwich <s235102@dtu.dk>
2025-02-19 20:36:19 +0100

    created an header and footer, also deleted/changed the default files for a deafult react project

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer(){
+
+    return(
+        <footer>
+            <p> &copy; {new Date().getFullYear()} GameTogether </p>
+        </footer>
+    );
+
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+
+    return(
+        <header>
+            <h1> My website </h1>
+            <nav>
+                <ul>
+                    <li> <a href = "#" > GameTogether </a> </li>
+                    <li> <a href = "#" > Search </a> </li>
+                    <li> <a href = "#" > Profile </a> </li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+
+}
+
+export default Header

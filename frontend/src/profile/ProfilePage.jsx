fce44a9657ab04fdc04e3ab2a94693eb72e8aba5
frede <s224804@dtu.dk>
2025-04-06 19:33:58 +0200

    better styling. new background color

--- frontend/src/profile/ProfilePage.jsx
+++ frontend/src/profile/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {fetchUserProfile} from "../services/apiService.js";
+import "./ProfilePage.css";
 
 
 const UserProfilePage = () => {

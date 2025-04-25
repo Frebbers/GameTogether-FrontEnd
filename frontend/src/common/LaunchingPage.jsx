f80c490248c639172af85385ade133ebd88320f8
Mads Hartwich <s235102@dtu.dk>
2025-04-25 10:59:22 +0200

    launching page background and set the filter based on click

--- frontend/src/common/LaunchingPage.jsx
+++ frontend/src/common/LaunchingPage.jsx
@@ -2,17 +2,31 @@ import { useNavigate } from "react-router-dom";
 import logo1 from "../images/logo.png";
 import dd from "../images/d&d.png";
 import games from "../images/games.png";
+import background from "../images/background.png";
 
-const LaunchingPage = () => {
+const LaunchingPage = (setFilterTag) => {
+    const navigate = useNavigate();
     return (
-        <div className="launching-page-container">
-            <img src={logo1} alt="Launching Banner" className="launching-logo" />
-            
-            <div className="game-banners">
-                <img src={dd} alt="D&D Banner" className="game-banner" />
-                <img src={games} alt="Games Banner" className="game-banner" />
-            </div>
+        <div
+        className="launching-page-container"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat'
+        }}
+      >
+        <img src={logo1} alt="Launching Banner" className="launching-logo" />
+        <div className="launching-page-text">
+        <p>Your go-to place to find the best teammates to fit your needs and have a memorable time with.</p>
         </div>
+        <hr></hr>
+        <div className="game-banners">
+          <img src={dd} alt="D&D Banner" className="game-banner" onClick={() => setFilterTag("D&D")} />
+          <img src={games} alt="Games Banner" className="game-banner" onClick={() => setFilterTag("Other Game")} />
+        </div>
+      </div>
+      
     );
 };
 
